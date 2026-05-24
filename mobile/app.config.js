7f6f23f7381e3eaad2e11efdaaa4dfc9e11f4bc9
Saawan Mehta <mehta.saawan@gmail.com>
2026-05-23 22:39:28 -0400

    Use EAS file env var for Android google services config

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -6,7 +6,12 @@ module.exports = ({ config }) => {
   if (!expoConfig.android) {
     expoConfig.android = {};
   }
-  expoConfig.android.googleServicesFile = "./google-services.json";
+
+  // In EAS Build, GOOGLE_SERVICES_JSON can be configured as a secret file
+  // environment variable. Expo sets the variable value to the temp file path
+  // on the build worker. Locally, keep using the checked-out file.
+  expoConfig.android.googleServicesFile =
+    process.env.GOOGLE_SERVICES_JSON ?? "./google-services.json";
 
   return expoConfig;
 };

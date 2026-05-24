8c13eb733002ccff6dc195b3722cb8d7c0f04b14
Saawan Mehta <mehta.saawan@gmail.com>
2026-05-24 12:03:53 -0400

    Set Android compile SDK 36

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -8,5 +8,19 @@ module.exports = ({ config }) => {
   }
   expoConfig.android.googleServicesFile = process.env.GOOGLE_SERVICES_JSON || "./google-services.json";
 
+  
+  expoConfig.plugins = [
+    ...(expoConfig.plugins || []),
+    [
+      "expo-build-properties",
+      {
+        android: {
+          compileSdkVersion: 36,
+          buildToolsVersion: "36.0.0"
+        }
+      }
+    ]
+  ];
+
   return expoConfig;
 };
